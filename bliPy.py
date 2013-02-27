bd2e0ef4ab551eb9aae62375d086f0ad4a8fb496
Romain Bazile <gromain.baz@gmail.com>
2013-02-27 19:09:42 +0100

    Class update
    Added color dictionnary
    Added functions breathrand fade fadeseq fadetorand rand and blink
    Style update

--- bliPy.py
+++ bliPy.py
@@ -1,60 +1,100 @@
 #!/usr/bin/env python
 
-import time, sys, os, subprocess
+import time
+import sys
+import os
+import subprocess
+import random
 
-path = '/home/diogo/i386/blink1/commandline/blink1-tool'
+path = '/home/pi/blink1/blink1-tool'  #TO BE REPLACED BY YOUR OWN BLINK1-TOOL ADDRESS
 
-cmd_on      = ' --on'
-cmd_off     = ' --off'
-cmd_red     = ' --red'
-cmd_green   = ' --green'
-cmd_blue    = ' --blue'
-cmd_rgb     = ' --rgb '
-cmd_blink   = ' --blink '
-cmd_random  = ' --random '
-cmd_list    = ' --list'
+cmd_on = ' --on'
+cmd_off = ' --off'
+cmd_red = ' --red'
+cmd_green = ' --green'
+cmd_blue = ' --blue'
+cmd_rgb = ' --rgb '
+cmd_blink = ' --blink '
+cmd_random = ' --random '
+cmd_list = ' --list'
 cmd_version = ' --version'
 
-opt_fade    = ' -m ' # written in milliseconds
-opt_timing  = ' -t ' # written in milliseconds
+opt_fade = ' -m '       # written in milliseconds
+opt_timing = ' -t '     # written in milliseconds
 
 
 def emitCommand(command):
-
     output = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     output = output.stdout.read()
     return output
 
 
 class Blink1:
-
     def __init__(self):
         output = emitCommand(path + cmd_list)
         if (output[:2] == 'no'):
-		    raise NameError('No blink1 Device Found!')
+            raise NameError('No blink1 Device Found!')
 
-    def breath(self, interval, rgb = None):
-        command = path + opt_fade + str(interval * 1000) + cmd_off
+    def breath(self, interval, rgb=None):
+        command = path + opt_fade + str(interval * 1000 / 2) + cmd_off
         emitCommand(command)
-        time.sleep(interval)
-        if(rgb != None):
+        time.sleep(interval / 2)
+        if(rgb is not None):
             color = ','.join(str(x) for x in rgb)
-            command = path + opt_fade + str(interval * 1000) + cmd_rgb + color
+            command = (path + opt_fade + str(interval * 1000 / 2) + cmd_rgb
+                       + color)
         else:
-		    command = path + opt_fade + str(interval * 1000) + cmd_on
-
+            command = path + opt_fade + str(interval * 1000 / 2) + cmd_on
         emitCommand(command)
-        time.sleep(interval)
 
+    def breathrand(self, interval):
+        command = path + opt_fade + str(interval * 1000 / 2) + cmd_off
+        emitCommand(command)
+        time.sleep(interval / 2)
+        rgb = Blink1.colors[random.choice(Blink1.colors.keys())]
+        color = ','.join(str(x) for x in rgb)
+        command = path + opt_fade + str(interval * 1000 / 2) + cmd_rgb + color
+        emitCommand(command)
 
-if __name__ == '__main__':
+    def fade(self, interval, rgb=None):
+        if(rgb is not None):
+            color = ','.join(str(x) for x in rgb)
+            command = (path + opt_fade + str(interval * 1000 / 2) + cmd_rgb
+                       + color)
+        else:
+            command = path + opt_fade + str(interval * 1000 / 2) + cmd_on
+        emitCommand(command)
 
-    bl1 = Blink1()	
+    def fadeseq(self, interval):
+        for color in Blink1.colors.keys():
+            self.fade(interval, Blink1.colors[color])
+            time.sleep(interval)
 
-    time_interval = 1.75
+    def fadetorand(self, interval):
+        rgb = Blink1.colors[random.choice(Blink1.colors.keys())]
+        color = ','.join(str(x) for x in rgb)
+        command = path + opt_fade + str(interval * 1000 / 2) + cmd_rgb + color
+        emitCommand(command)
 
-    while 1:
-        bl1.breath(time_interval)
+    def rand(self, interval):
+        command = path + opt_fade + str(interval * 1000 / 2) + cmd_random
+        emitCommand(command)
 
+    def blink(self, interval):
+        command = path + opt_fade + str(interval * 1000 / 2) + cmd_blink + '1'
+        emitCommand(command)
 
-
+    colors = {'RED': [255, 0, 0],
+              'ORANGE': [255, 125, 0],
+              'YELLOW': [255, 255, 0],
+              'SPRING_GREEN': [125, 255, 0],
+              'GREEN': [0, 255, 0],
+              'TURQUOISE': [0, 255, 125],
+              'CYAN': [0, 255, 255],
+              'OCEAN': [0, 125, 255],
+              'BLUE': [0, 0, 255],
+              'VIOLET': [125, 0, 255],
+              'MAGENTA': [255, 0, 255],
+              'RASPBERRY': [255, 0, 125],
+              'WHITE': [255, 255, 255],
+              'GREY': [125, 125, 125]}
